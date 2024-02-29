93f6c3ffae61d4b0633fd06ffe5b444a4e69c670
algebnaly <algebnaly@qq.com>
2024-02-29 22:27:31 +0800

    add note on config.lua

--- Config.lua
+++ Config.lua
@@ -14,7 +14,7 @@ return {
     },
     Description = [[
         简单地减少小黑蛇修改正逆书页所需的历练
-        原本的修改一页所需的历练为读一页所获取的历练的20倍, 但是因为后期历练获取量少, 因此改为了5倍
-        Github: https://github.com/yalikes/
+        原本的修改一页所需的历练为读一页所获取的历练的20倍, 但是因为后期历练获取量少, 因此改为了5倍方便提升游戏体验
+        Github: https://github.com/yalikes/TaiwuLittleBlackSnakeModify
         ]]
 }
